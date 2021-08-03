883fa6ef6fa089c909125be0dca556ee48121782
Ron <nguyen.thanh.duc@moneyforward.vn>
2021-08-03 07:53:30 +0700

    move home to book index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   post 'books/search', to: 'books#search'
 
-  root to: 'static_pages#home'
+  root to: 'books#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
